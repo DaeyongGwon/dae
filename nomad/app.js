125c72502f8f841f5a7b881af2467b868eb9dc16
daeyong <cys964@gmail.com>
2023-05-07 18:50:27 +0900

    querySelector, querySelectorAll의 차이점과 배열 확인 중

--- nomad/app.js
+++ nomad/app.js
@@ -1,1 +1,5 @@
-document.title = "hello";
+const title = document.querySelector(".hello h1");
+console.log(title.textContent);
+
+const titles = document.querySelectorAll(".hello h1");
+console.log(titles[2].textContent);
