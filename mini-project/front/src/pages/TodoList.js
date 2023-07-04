7a809de8d490706ac04c09dfc26711a269914ce8
dae <cys964@gmail.com>
2023-07-04 09:30:58 +0900

    ddd

--- mini-project/front/src/pages/TodoList.js
+++ mini-project/front/src/pages/TodoList.js
@@ -6,6 +6,8 @@ import { SERVER } from "../lib/config";
 function Todos() {
     const [todos, setTodos] = useState([]);
     const { register, handleSubmit, reset } = useForm();
+    const [editTodoId, setEditTodoId] = useState(null);
+    const [editing, setEditing] = useState(false);
 
     const onValid = async (data) => {
         try {
@@ -13,6 +15,21 @@ function Todos() {
 
             setTodos((prevTodos) => [...prevTodos, response.data]);
             reset(); // 입력 필드 리셋
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const handleTodoEdit = async (todoId, newTitle) => {
+        try {
+            await axios.patch(`${SERVER}/todo/${todoId}`, { title: newTitle });
+            setTodos((prevTodos) =>
+                prevTodos.map((todo) => {
+                    if (todo.id === todoId) {
+                        return { ...todo, title: newTitle };
+                    }
+                    return todo;
+                })
+            );
         } catch (error) {
             console.log(error);
         }
@@ -38,25 +55,15 @@ function Todos() {
         }
     };
 
-    const handleTodoDoubleClick = async (todoId) => {
-        const newTitle = prompt("수정할 내용을 입력하세요");
-        if (newTitle) {
-            try {
-                await axios.patch(`${SERVER}/todo/${todoId}`, { title: newTitle });
-                setTodos((prevTodos) =>
-                    prevTodos.map((todo) => {
-                        if (todo.id === todoId) {
-                            return { ...todo, title: newTitle };
-                        }
-                        return todo;
-                    })
-                );
-            } catch (error) {
-                console.log(error);
-            }
-        }
+    const handleTodoDoubleClick = (todoId) => {
+        setEditTodoId(todoId); // 선택된 투두 아이템의 ID 저장
+        setEditing(true); // 편집 모드로 변경
     };
 
+    const handleTodoBlur = (todoId, newTitle) => {
+        setEditing(false); // 편집 모드 종료
+        handleTodoEdit(todoId, newTitle); // 수정된 내용을 서버에 저장
+    };
     const handleTodoDelete = async (todoId) => {
         try {
             await axios.delete(`${SERVER}/todo/${todoId}`);
@@ -65,6 +72,7 @@ function Todos() {
             console.log(error);
         }
     };
+
     useEffect(() => {
         async function fetchData() {
             try {
@@ -81,16 +89,8 @@ function Todos() {
     return (
         <div className="Todo">
             <h1>TodoList</h1>
-            <form onSubmit={handleSubmit(onValid, onInvalid)}>
-                <input
-                    {...register("title", { required: "Add your new Todo" })}
-                    type="text"
-                    placeholder="Add your new Todo"
-                    className="todo-input"
-                />
-                <button type="submit">등록</button>
-            </form>
-            <ul>
+
+            <ul className="TodoUl">
                 {todos.map((todo) => (
                     <li key={todo.id} className="TodoList">
                         <input
@@ -98,13 +98,46 @@ function Todos() {
                             defaultChecked={todo.done === 1}
                             onChange={(e) => handleCheckboxChange(todo.id, e.target.checked)}
                         />
-                        <span onDoubleClick={() => handleTodoDoubleClick(todo.id)}>{todo.title}</span>
+                        {editing && editTodoId === todo.id ? (
+                            <input
+                                className="TodoEdit"
+                                type="text"
+                                defaultValue={todo.title}
+                                onBlur={(e) => handleTodoBlur(todo.id, e.target.value)} // 입력 필드에서 포커스를 잃을 때 편집 모드 종료
+                                onKeyPress={(e) => {
+                                    if (e.key === "Enter") {
+                                        handleTodoBlur(todo.id, e.target.value); // Enter 키를 누를 때 편집 모드 종료하고 수정된 내용을 서버에 저장
+                                    }
+                                }}
+                            />
+                        ) : (
+                            <span className="TodoTitle" onDoubleClick={() => handleTodoDoubleClick(todo.id)}>
+                                {todo.title}
+                            </span>
+                        )}
                         <button onClick={() => handleTodoDelete(todo.id)} className="deleteBtn">
                             삭제
                         </button>
                     </li>
                 ))}
             </ul>
+            <div className="layout">
+                <div className="submitBox">
+                    <h3 className="TodoBoxText">＂오늘의 할 일을 기록해보세요＂</h3>
+                    <form className="TodoForm" onSubmit={handleSubmit(onValid, onInvalid)}>
+                        <input
+                            {...register("title", { required: "Add your new Todo" })}
+                            type="text"
+                            placeholder="Add your new Todo"
+                            className="todo-input"
+                        />
+                        <button type="submit" className="TodoCreate">
+                            등록
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div className="background"></div>
         </div>
     );
 }
--- mini-project/front/src/setupProxy.js
+++ mini-project/front/src/setupProxy.js
@@ -1,18 +1,18 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//     //get Todo, create Todo
-//     app.use(
-//         createProxyMiddleware("/todo", {
-//             target: "http://localhost:8000",
-//             changeOrigin: true,
-//         })
-//     );
-//     // delete Todo, update Todo
-//     app.use(
-//         createProxyMiddleware("/todo/:todoId", {
-//             target: "http://localhost:8000",
-//             changeOrigin: true,
-//         })
-//     );
-// };
+module.exports = function (app) {
+    //get Todo, create Todo
+    app.use(
+        createProxyMiddleware("/todo", {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+        })
+    );
+    // delete Todo, update Todo
+    app.use(
+        createProxyMiddleware("/todo/:todoId", {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+        })
+    );
+};
