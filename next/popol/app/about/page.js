3542f6fb817b08342845b345cc491d2b24131c06
dae <cys964@gmail.com>
2023-07-29 20:09:43 +0900

    포트폴리오 페이지 만들기

---
+++ next/popol/app/about/page.js
@@ -0,0 +1,13 @@
+export default function about() {
+    return (
+        <div>
+            <h1 className="title">- 권대용 -</h1>
+            <hr className="underLine"></hr>
+            <div className="title-sub">
+                안녕하세요. <br />
+                공부가 취미인 풀 스택 웹 개발자입니다. <br />
+                매우 꼼꼼한 성격, 그리고 공부를 밥 먹듯이 하는 습관이 저의 장점입니다.
+            </div>
+        </div>
+    );
+}
