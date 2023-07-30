1262800246b0905187087171836782bc1fd8c8aa
dae <cys964@gmail.com>
2023-07-30 21:30:59 +0900

    왜 안돼

--- next/popol/app/page.js
+++ next/popol/app/page.js
@@ -11,10 +11,9 @@ export default function Home() {
                 </h1>
                 <hr className="underLine"></hr>
                 <div className="title-sub">
-                    {/* 안녕하세요. <br />
+                    안녕하세요. <br />
                     공부가 취미인 풀 스택 웹 개발자입니다. <br />
-                    매우 꼼꼼한 성격, 그리고 공부를 밥 먹듯이 하는 습관이 저의 장점입니다. */}
-                    안녕하세요 안녕하세요 안녕하세요
+                    매우 꼼꼼한 성격, 그리고 공부를 밥 먹듯이 하는 습관이 저의 장점입니다. 안녕하세요 안녕하세요 안녕하세요
                 </div>
             </div>
             <div className="about-mainBox">
